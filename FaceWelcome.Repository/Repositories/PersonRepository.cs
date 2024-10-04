6fcc2d5623f713e94f4ecf94c6fc4678890f65ae
Phạm Cường <phamcuongtran222@gmail.com>
2024-10-04 12:07:06 +0700

    add get all people

--- FaceWelcome.Repository/Repositories/PersonRepository.cs
+++ FaceWelcome.Repository/Repositories/PersonRepository.cs
@@ -16,11 +16,35 @@ namespace FaceWelcome.Repository.Repositories
             _dbContext = dbContext;
         }
 
+        public async Task AddAsync(Person person)
+        {
+            try
+            {
+                _dbContext.People.AddAsync(person);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
         public async Task<Person> GetPersonByIdAsync(Guid id)
         {
             try
             {
                 return await _dbContext.People.Include(person => person.Organization).SingleOrDefaultAsync(p => p.Id == id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<List<Person>> GetPeopleAsync()
+        {
+            try
+            {
+                return await _dbContext.People.Include(p => p.Organization).ToListAsync();
             }
             catch (Exception ex)
             {
--- FaceWelcome.Service/Services/Implementations/PersonService.cs
+++ FaceWelcome.Service/Services/Implementations/PersonService.cs
@@ -1,9 +1,13 @@
 ﻿using AutoMapper;
+using Azure.Core;
 using FaceWelcome.Repository.Infrastructures;
+using FaceWelcome.Repository.Models;
+using FaceWelcome.Service.DTOs.Request.Person;
 using FaceWelcome.Service.DTOs.Response.Person;
 using FaceWelcome.Service.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +24,80 @@ namespace FaceWelcome.Service.Services.Implementations
             this._unitOfWork = (UnitOfWork)unitOfWork;
             this._mapper = mapper;
         }
+        #region Create Person
+        public async Task CreatePersonAsync(PostPersonRequest postPersonRequest)
+        {
+            // Kiểm tra file có tồn tại không
+            if (postPersonRequest.Image == null || postPersonRequest.Image.Length == 0)
+            {
+                throw new InvalidOperationException("No image file provided.");
+            }
+
+            // Tạo tên file tạm thời duy nhất
+            var tempFilePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + Path.GetExtension(postPersonRequest.Image.FileName));
+            try
+            {
+                // Lưu file tạm thời
+                using (var stream = new FileStream(tempFilePath, FileMode.Create))
+                {
+                    await postPersonRequest.Image.CopyToAsync(stream); // Sử dụng ImageFile từ request
+                }
+
+                // Tải lên hình ảnh và lấy URL từ Firebase
+                string imageUrl = await _unitOfWork.FirebaseStorageRepository.UploadImageToFirebase(tempFilePath, "persons");
+
+                //Tạo đối tượng Person
+                var person = new Person
+                {
+                    Image = imageUrl,
+                    Address = postPersonRequest.Address,
+                    Code = postPersonRequest.Code,
+                    DateOfBirth = postPersonRequest.DateOfBirth,
+                    Email1 = postPersonRequest.Email1,
+                    Email2 = postPersonRequest.Email2,
+                    FullName = postPersonRequest.FullName,
+                    Gender = postPersonRequest.Gender,
+                    Phone = postPersonRequest.Phone,
+                    Position = postPersonRequest.Position,
+                    OrganizationId = postPersonRequest.OrganizationId,
+                };
+                await _unitOfWork.PersonRepository.AddAsync(person);
+                await _unitOfWork.CommitAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        #endregion
+
+        #region Get all people
+        public async Task<GetPeopleResponse> GetAllPersonsAsync(GetPeopleRequest getPeopleRequest)
+        {
+            var people = await _unitOfWork.PersonRepository.GetPeopleAsync();
+            if (people == null)
+            {
+                throw new Exception();
+            }
+            int totalRecords = people.Count;
+            int totalPages = (int)System.Math.Ceiling((double)totalRecords / getPeopleRequest.PageSize);
+            var list = people.Skip((getPeopleRequest.PageNumber - 1) * getPeopleRequest.PageSize)
+               .Take(getPeopleRequest.PageSize)
+               .ToList();
+            var responseList = this._mapper.Map<List<GetPersonResponse>>(list);
+
+            var response = new GetPeopleResponse
+            {
+                PageSize = getPeopleRequest.PageSize,
+                TotalRecords = totalRecords,
+                TotalPages = totalPages,
+                PageNumber = getPeopleRequest.PageNumber,
+                Persons = responseList
+            };
+            return response;
+        }
+        #endregion
+
         public async Task<GetPersonResponse> GetPersonByIdAsync(Guid id)
         {
             try
@@ -37,5 +115,7 @@ namespace FaceWelcome.Service.Services.Implementations
                 throw new Exception(ex.Message);
             }
         }
+
+
     }
 }

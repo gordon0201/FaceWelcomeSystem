5f50d61181feac4c5064b2c94d8dc7a864e4c88a
GordonDang <cuongdhtse184437@fpt.edu.vn>
2024-10-09 12:03:14 +0700

    crud guestImage

---
+++ FaceWelcome.Service/DTOs/Request/GuestImage/UpdateGuestImageRequest.cs
@@ -0,0 +1,17 @@
+﻿    using Microsoft.AspNetCore.Http;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace FaceWelcome.Service.DTOs.Request.GuestImage
+    {
+        public class UpdateGuestImageRequest
+        {
+            public string type { get; set; }
+            public IFormFile ImageFile { get; set; }
+            public string description { get; set; }
+            public Guid guestId { get; set; }
+        }
+    }
---
+++ FaceWelcome.Service/DTOs/Response/GuestImage/GetAllGuestImagesResponse.cs
@@ -0,0 +1,18 @@
+﻿using FaceWelcome.Service.DTOs.Response.Group;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceWelcome.Service.DTOs.Response.GuestImage
+{
+    public class GetAllGuestImagesResponse
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalRecords { get; set; }
+        public int TotalPages { get; set; }
+        public List<GetGuestImageResponse> GuestImages { get; set; }
+    }
+}
